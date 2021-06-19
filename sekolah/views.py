9ca2c15046f0fc91bada3dba28537f547588cc45
azzerative <salehzaidan53@gmail.com>
2021-06-19 15:37:03 +0700

    Set up urls

--- sekolah/views.py
+++ sekolah/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'sekolah/home.html', context=None)
+
+
+def year(request):
+    pass
+
+
+def group(request):
+    pass
+
+
+def student(request):
+    pass
